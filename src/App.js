f9b603e0564c28a7823214eac31c905fc3aded66
James Smith <33375554+sportschord@users.noreply.github.com>
2022-01-06 21:37:08 +0000

    adjust bg gradient

--- src/App.js
+++ src/App.js
@@ -10,44 +10,53 @@ function App() {
   const [newdat, setNewData] = useState([]);
   const [pos, setPos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedOption, setSelectedOption] = useState("2010-2021")
   const ref = useRef();
+
+  console.log(selectedOption);
 
   useEffect(() => {
 
-    console.log(streams);
-
     Promise.all([
-
-      
       d3.csv(streams),
       d3.csv(positions)
     ]).then((d) => {
-      
       return (
-        
         setNewData(d[0]),
         setPos(d[1])
-        )
-        
-    }).then(() =>
-        
-      setLoading(false)
-
       )
-    
+    }).then(() =>
+      setLoading(false)
+    )
   }, [])
 
-  console.log(newdat);
-
-  var period = new Set(pos.map(d=>d.period))
+  var period = new Set(pos.map(d => d.period))
+  
+  console.log(pos,newdat);
   
   var points = d => d.points
   var teams = d => d.team
   var season = d => d.season
 
+  let merged = [];
+
+  let newpos = pos.filter(d => d.period === selectedOption)
+
+  var maxpoints = newpos.map(d => parseInt(d.points)).reduce((a, b) => a + b,0)
+
+  console.log();
+
+  for (let i = 0; i < newdat.length; i++) {
+    merged.push({
+      ...newdat[i],
+      ...(pos.filter(d=>d.period===selectedOption).find((d) => d.position === newdat[i].position))
+    }
+    );
+  }
+
+  console.log('merged,',merged);
   
-  
-  var dat = newdat.map(a =>
+  var dat = merged.map(a =>
     ({
       constructor: a.constructor,
       season: a.season,
@@ -70,11 +79,9 @@ function App() {
         
       }));
   
-  console.log(d3.max(dat.map(d=>d.points)));
+  const handleSelect = (e) => setSelectedOption(e.target.value)
       
   var conpoints = d3.flatRollup(dat, v => d3.sum(v, points), teams).map(d => ({ team: d[0], points: d[1] }));
-
-  
   var sortedcons = conpoints.slice().sort((a, b) => d3.descending(a.points, b.points))
   // sortedcons = sortedcons.map(d => d.points > 100 ? { team: d.team } : { team: 'Others' });
 
@@ -113,7 +120,7 @@ function App() {
   // var keys = dataset.columns?.slice(2, dataset.length)
   var keys = [...cons]
   
-  console.log(keys);
+  // console.log(keys);
 
   let winwidth = window.innerWidth;
 
@@ -123,8 +130,13 @@ function App() {
     width = winwidth - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
   
-  var maxdom = 1200
+  console.log(maxpoints);
 
+  var races = 12
+  
+  var maxdom = maxpoints*races
+
+  console.log();
   var xScale = d3.scaleLinear().domain(d3.extent(seasons)).range([0, width])
   var yScale = d3.scaleLinear().domain([-maxdom, maxdom]).range([height, 0])
 
@@ -137,12 +149,11 @@ function App() {
 
       let data = await newm
 
-      console.log(newm);
+      // console.log(newm);
 
       // data = datatemp.map(({ position, ...item }) => item)
 
       var svg = d3.select(ref.current)
-        // .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -157,35 +168,44 @@ function App() {
       
 
       // Three function that change the tooltip when user hover / move / leave a cell
-      var mouseover = (d) => {
+      var mouseover = (d, e) => {
         Tooltip.style("opacity", 1)
         d3.selectAll(".myArea")
           .style("opacity", .1)
-        // d3.selectAll("circle")
-        //   .style("opacity", .1)
+        d3.selectAll("circle")
+          .style("opacity", .1)
+        d3.selectAll("text")
+          .style("opacity", .1)
+        d3.select("text." + d.path[0].id)
+          .transition().duration(100)
+          .style("opacity", 1)
+        d3.select("circle." + d.path[0].id)
+          .transition().duration(300)
+          .style("opacity", 1)
         d3.select("#"+d.path[0].id)
           .style("stroke", "black")
           .style("opacity", 1)
-        console.log(d3.select(d.id));
-        console.log(d.path[0].id);
+
       }
-      var mousemove = function (d, i) {
-        var grp = keys[i]
-        Tooltip.text(grp)
-      }
+      // var mousemove = function (d, i) {
+      //   var grp = keys[i]
+      //   Tooltip.text(grp)
+      // }
       var mouseleave = (d) => {
         Tooltip.style("opacity", 0)
         d3.selectAll(".myArea")
           .style("opacity", 1)
-          .style("stroke", "black")
+          .style("stroke", "none")
         d3.selectAll("circle")
+          .style("opacity", 1)
+        d3.selectAll("text")
           .style("opacity", 1)
       }
 
       var Tooltip = svg
         .append("text")
-        .attr("x", 10)
-        .attr("y", 10)
+        .attr("x", 50)
+        .attr("y", 50)
         .style("opacity", 0)
         .style("color", "white")
         .style("font-size", 17)
@@ -200,12 +220,13 @@ function App() {
         // Show the areas
         svg.selectAll("mylayers")
           .data(stackedData)
-          .enter()
-          .append("path")
+          .join("path")
+          // .transition()
+          // .duration(100)
           .attr("class", "myArea")
           .style("fill", d => teamcols[d.key])
           // .style("stroke", "black")
-          .attr("id",d => d.key.replace(" ",""))
+          .attr("id", d => d.key.replace(/ /g,""))
           .attr("d", area)
           .on("mouseover", mouseover)
           // .on("mousemove", mousemove)
@@ -214,54 +235,61 @@ function App() {
 
     }
     
-      init()
+    init()
+    
     
       
-    }, [loading]);
-
+  }, [loading,selectedOption]);
   
-    var cols = 9
-    var gap = width / cols
+  var cols = 9
+  var gap = width / cols
+
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-        
+
         {loading ? <p>loading</p> :
           <div>
-            <p>Formula 1 Constructors <br/> 1950-2021</p>
-            {/* <select>
-              {[...period].map(d => <option value={d} key={d}>{d}</option>)}  
-            </select> */}
+            <p>Formula 1 Constructors <br /> 1950-2021</p>
+            {/* <select onChange={handleSelect} value={selectedOption}>
+              {[...period].map(d =>
+                <option value={d} key={d} >
+                {d}
+                </option>)}  
+            </select>
+            <div>
+              {pos.filter(d=>d.period===selectedOption).map(d =>
+                <p fill="white" style={{ fontSize: "9px" }} key={d.position}>{d.position} : {d.points}</p>)}
+            </div> */}
           </div>
         }
         <svg ref={ref} width={width} height={height}>
           <text fill='white' x={50} y={330} fontSize={10}>Size of stream represents number of points</text>
           <text fill='white' x={40} y={150} fontSize={10}>1950</text>
-          <text fill='white' x={width+30} y={50} fontSize={10}>2021</text>
+          <text fill='white' x={width + 30} y={50} fontSize={10}>2021</text>
         </svg>
-        {/* <text>{Tooltip}</text> */}
-
-        <svg width={width}>          
+        <svg width={width}>
           {keys?.map((d, i) =>
             <g key={d + i}>
-            <circle
+              <circle
                 key={d + i}
-                id={d.replace(" ", "")}
-                className={d.replace(" ", "")}
-                cx={margins+(i % cols) * (gap)}
-                cy={20 + (Math.floor(i/cols))*20}
+                id={d.replace(/ /g, "")}
+                className={d.replace(/ /g, "")}
+                cx={margins/2 + (i % cols) * (gap)}
+                cy={20 + (Math.floor(i / cols)) * 20}
                 r={5}
                 fill={teamcols[d]}
-                // stroke={'white'}
               >
               </circle>
               <text
-                x={margins + (i % cols) * (gap) + 10}
+                x={margins/2 + (i % cols) * (gap) + 10}
                 y={3 + (20 + (Math.floor(i / cols)) * 20)}
-                fontSize={10}
-              fill={"white"}
+                fontSize={9}
+                className={d.replace(/ /g, "")}
+                fill={"white"}
               >
                 {d}
               </text>
