47bda3a95b2c6d19ecd13baa41397a3129f1075b
James Smith <33375554+sportschord@users.noreply.github.com>
2022-01-29 10:03:10 +0000

    add points system functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import * as d3 from 'd3'
-import teamcols from './teamcols.json'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import { useEffect, useRef, useState } from 'react';
+import teamcols from './teamcols.json'
 import streams from './f1stream.csv'
 import positions from './positions.csv'
 
@@ -13,8 +18,8 @@ function App() {
   const [selectedOption, setSelectedOption] = useState("2010-2021")
   const ref = useRef();
 
-  console.log(selectedOption);
-
+  // first useEffect to get data from csv
+  
   useEffect(() => {
 
     Promise.all([
@@ -30,21 +35,17 @@ function App() {
     )
   }, [])
 
+  const handleSelect = (e) => setSelectedOption(e.target.value)
+
+  // get the different periods from points csv
   var period = new Set(pos.map(d => d.period))
   
-  console.log(pos,newdat);
-  
+  //accessor functions
   var points = d => d.points
   var teams = d => d.team
   var season = d => d.season
 
   let merged = [];
-
-  let newpos = pos.filter(d => d.period === selectedOption)
-
-  var maxpoints = newpos.map(d => parseInt(d.points)).reduce((a, b) => a + b,0)
-
-  console.log();
 
   for (let i = 0; i < newdat.length; i++) {
     merged.push({
@@ -53,8 +54,6 @@ function App() {
     }
     );
   }
-
-  console.log('merged,',merged);
   
   var dat = merged.map(a =>
     ({
@@ -79,11 +78,18 @@ function App() {
         
       }));
   
-  const handleSelect = (e) => setSelectedOption(e.target.value)
+  
       
   var conpoints = d3.flatRollup(dat, v => d3.sum(v, points), teams).map(d => ({ team: d[0], points: d[1] }));
-  var sortedcons = conpoints.slice().sort((a, b) => d3.descending(a.points, b.points))
-  // sortedcons = sortedcons.map(d => d.points > 100 ? { team: d.team } : { team: 'Others' });
+  var sortedcons = conpoints.slice().sort((a, b) => d3.descending(a.points, b.points));
+  sortedcons = sortedcons.filter(d => d.points > 0);
+
+
+
+  console.log(sortedcons.filter(d => d.points > 0));
+  
+  var seaspoints = d3.flatRollup(dat, v => d3.sum(v, points), season).map(d => ({ season: d[0], points: d[1] }));
+  var maxpoints = d3.max(seaspoints.map(d => d.points));
 
   var cons = sortedcons.map(d => d.team)
   var seasons = Array.from(new Set(dat.map(season)));
@@ -114,13 +120,10 @@ function App() {
 
   }
 
-  
 
+  var keys = sortedcons.map(d => d.team)
   
-  // var keys = dataset.columns?.slice(2, dataset.length)
-  var keys = [...cons]
-  
-  // console.log(keys);
+  console.log(keys);
 
   let winwidth = window.innerWidth;
 
@@ -130,43 +133,38 @@ function App() {
     width = winwidth - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
   
-  console.log(maxpoints);
+    
 
-  var races = 12
+  var maxdom = maxpoints/2
   
-  var maxdom = maxpoints*races
-
-  console.log();
+    
+    
   var xScale = d3.scaleLinear().domain(d3.extent(seasons)).range([0, width])
   var yScale = d3.scaleLinear().domain([-maxdom, maxdom]).range([height, 0])
-
+  
+  var stackedData = d3.stack()
+  .offset(d3.stackOffsetSilhouette)
+  .keys(keys)
+  (newm)
+  
+  var area = d3.area()
+  .x(d => xScale(d.data.season))
+  .y0(d => yScale(d[0]))
+  .y1(d => yScale(d[1]))
+  .curve(d3.curveCardinal);
   
   useEffect(() => {
-
-    async function init() {
-
-      // let data = await dataset
-
-      let data = await newm
-
-      // console.log(newm);
-
-      // data = datatemp.map(({ position, ...item }) => item)
-
+    
+    if (newm) {
+      
+      
       var svg = d3.select(ref.current)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-     
-      var stackedData = d3.stack()
-          .offset(d3.stackOffsetSilhouette)
-          .keys(keys)
-          (data)
       
-      
-
       // Three function that change the tooltip when user hover / move / leave a cell
       var mouseover = (d, e) => {
         Tooltip.style("opacity", 1)
@@ -176,21 +174,18 @@ function App() {
           .style("opacity", .1)
         d3.selectAll("text")
           .style("opacity", .1)
-        d3.select("text." + d.path[0].id)
-          .transition().duration(100)
+        d3.selectAll("text." + d.path[0].id)
+          // .transition().duration(100)
           .style("opacity", 1)
         d3.select("circle." + d.path[0].id)
-          .transition().duration(300)
+          // .transition().duration(300)
           .style("opacity", 1)
-        d3.select("#"+d.path[0].id)
+        d3.select("#" + d.path[0].id)
           .style("stroke", "black")
           .style("opacity", 1)
 
       }
-      // var mousemove = function (d, i) {
-      //   var grp = keys[i]
-      //   Tooltip.text(grp)
-      // }
+
       var mouseleave = (d) => {
         Tooltip.style("opacity", 0)
         d3.selectAll(".myArea")
@@ -211,35 +206,43 @@ function App() {
         .style("font-size", 17)
 
       
-      var area = d3.area()
-        .x(d => xScale(d.data.season))
-        .y0(d => yScale(d[0]))
-        .y1(d => yScale(d[1]))
-        .curve(d3.curveCardinal);
     
-        // Show the areas
-        svg.selectAll("mylayers")
-          .data(stackedData)
-          .join("path")
-          // .transition()
-          // .duration(100)
-          .attr("class", "myArea")
-          .style("fill", d => teamcols[d.key])
-          // .style("stroke", "black")
-          .attr("id", d => d.key.replace(/ /g,""))
-          .attr("d", area)
-          .on("mouseover", mouseover)
-          // .on("mousemove", mousemove)
-          .on("mouseleave", mouseleave)
-
-
-    }
-    
-    init()
-    
-    
+      // Show the areas
+      svg.selectAll("mylayers")
+        .data(stackedData)
+        .enter()
+        .append("path")
+              .attr("d", area)
+              .attr("class", "myArea")
+              .style("fill", d => teamcols[d.key])
+              .attr("id", d => d.key.replace(/ /g, ""))
+              .on("mouseover", mouseover)
+              .on("mouseleave", mouseleave)         
       
-  }, [loading,selectedOption]);
+      }
+      
+    },
+    
+    [loading]);
+    
+    
+  // second useEffect to transtion the data when user changes the points filter
+  
+  useEffect(() => {
+
+      d3.select(ref.current)
+        .selectAll("path")
+        .data(stackedData)
+        .attr("id", d => d.key.replace(/ /g, ""))
+        .transition()
+        .ease(d3.easeCubicIn)
+        // .delay(500)
+        .duration(1000)
+        .attr("d", area)
+        .style("fill", d => teamcols[d.key])
+        
+    
+  },[selectedOption])
   
   var cols = 9
   var gap = width / cols
@@ -253,46 +256,82 @@ function App() {
 
         {loading ? <p>loading</p> :
           <div>
-            <p>Formula 1 Constructors <br /> 1950-2021</p>
-            {/* <select onChange={handleSelect} value={selectedOption}>
-              {[...period].map(d =>
-                <option value={d} key={d} >
-                {d}
-                </option>)}  
-            </select>
+            <h3>Formula 1 Constructors</h3>
+            <h5 className='subtitle'>POINTS SYSTEM</h5>
             <div>
+              {/* <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Points System</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={selectedOption}
+                    label={selectedOption}
+                    onChange={handleSelect}
+                  >
+                    {[...period].map(d =>
+                      <MenuItem value={d}>{d}</MenuItem>
+                    )}
+                  </Select>
+                </FormControl>
+              </Box> */}
+              
+              <select onChange={handleSelect} value={selectedOption} className='select'>
+                {[...period].map(d =>
+                  <option value={d} key={d} className='option'>
+                  {d}
+                  </option>)}  
+              </select>
+                  </div>
+            {/* <div>
               {pos.filter(d=>d.period===selectedOption).map(d =>
                 <p fill="white" style={{ fontSize: "9px" }} key={d.position}>{d.position} : {d.points}</p>)}
             </div> */}
+
           </div>
         }
+          
+        
         <svg ref={ref} width={width} height={height}>
           <text fill='white' x={50} y={330} fontSize={10}>Size of stream represents number of points</text>
           <text fill='white' x={40} y={150} fontSize={10}>1950</text>
-          <text fill='white' x={width + 30} y={50} fontSize={10}>2021</text>
+          <text fill='white' x={width + 30} y={42} fontSize={10}>2021</text>
         </svg>
+            
+          
         <svg width={width}>
-          {keys?.map((d, i) =>
-            <g key={d + i}>
+          {sortedcons?.map((d, i) =>
+            <g key={d.team + i}>
               <circle
-                key={d + i}
-                id={d.replace(/ /g, "")}
-                className={d.replace(/ /g, "")}
+                key={d.team + i}
+                id={d.team.replace(/ /g, "")}
+                className={d.team.replace(/ /g, "")}
                 cx={margins/2 + (i % cols) * (gap)}
                 cy={20 + (Math.floor(i / cols)) * 20}
                 r={5}
-                fill={teamcols[d]}
+                fill={teamcols[d.team]}
               >
               </circle>
               <text
                 x={margins/2 + (i % cols) * (gap) + 10}
                 y={3 + (20 + (Math.floor(i / cols)) * 20)}
                 fontSize={9}
-                className={d.replace(/ /g, "")}
+                className={d.team.replace(/ /g, "")}
                 fill={"white"}
               >
-                {d}
+                {d.team} 
               </text>
+
+              {winwidth > 1500 ?
+                <text
+                  x={margins / 2 + (i % cols) * (gap) + 92}
+                  y={3 + (20 + (Math.floor(i / cols)) * 20)}
+                  fontSize={9}
+                  className={d.team.replace(/ /g, "")}
+                  fill={"#d3d3d3"}
+                >
+                  {d.points.toLocaleString("en-US")}
+                </text> : ""}
             </g>
           )}
         </svg>
